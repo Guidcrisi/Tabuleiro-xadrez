35c479081072da4e811ba01324460a0a3ad5331e
Guilherme Crisi <guilherme.campos137@gmail.com>
2023-03-28 14:23:15 -0300

    Commit inicial

---
+++ components/Linha.jsx
@@ -0,0 +1,17 @@
+import Subdivisao from '../components/Subdivisao'
+import styles from '../styles/Linha.module.css'
+
+export default function Linhas(props) {
+    return (
+        <div className={styles.linha}>
+            <Subdivisao preta={props.preta} />
+            <Subdivisao preta={!props.preta} />
+            <Subdivisao preta={props.preta} />
+            <Subdivisao preta={!props.preta} />
+            <Subdivisao preta={props.preta} />
+            <Subdivisao preta={!props.preta} />
+            <Subdivisao preta={props.preta} />
+            <Subdivisao preta={!props.preta} />
+        </div>
+    )
+}
